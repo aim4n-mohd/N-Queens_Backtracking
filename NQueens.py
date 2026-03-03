0d8814bb2920cac743c34f75b5c48d75675a67a9
Aiman <aim4n.mohd@gmail.com>
2026-03-03 17:08:35 +0530

    2nd try - working

--- NQueens.py
+++ NQueens.py
@@ -1,48 +1,48 @@
-init_board = [[0,0,0,0],
-              [0,0,0,0],
-              [0,0,0,0],
-              [0,0,0,0]]
-N = 4
+N = int(input("How rows/cols/queens? "))
 
+board = [[0]*N for _ in range(N)]
 solutions = []
 
 def check(board, row, col):
-    
-
     for i in range(N):
         if i!=row:
             if board[i][col]:
                 return False
-    for i in range(N):
         if i!=col:
             if board[row][i]:
                 return False
-            
-    directions = ((-1,-1),(-1,1))
-        
-    for dr,dc in directions:
-        new_row = row + dr
-        new_col = col + dc
-        if 0<=new_row<row and 0<=new_col<col:
-            if board[new_row][new_col]:
+    
+    direction = [(-1,-1),(-1,1)]
+    
+    for dr,dc in direction:
+        r = dr + row
+        c = dc + col
+
+        while 0<=r<N and 0<=c<N:
+            if board[r][c]:
                 return False
+            r+=dr
+            c+=dc
+
     return True
 
 
 def queen(board, row, col=0):
-    
-    if check(board, row, col):
-        board[row][col] = 1
-    else:
-        return None
-    if row!=N-1:
-        for i in range(N):
-            queen(board, row+1, i)
-    return board
 
-for i in range(N):
-    board = [[0,0,0,0],
-              [0,0,0,0],
-              [0,0,0,0],
-              [0,0,0,0]]
-    print(queen(board, i))
+    if row==N:
+        solutions.append([r[:] for r in board])
+        return
+    
+    for col in range(N):
+        if check(board, row, col):
+            board[row][col] = 1
+            queen(board, row+1)
+            board[row][col] = 0
+
+
+queen(board, 0)
+for i,sol in enumerate(solutions):
+    print(f"\n\nSolution - {i+1}:")
+    for j in sol:
+        print(j)
+print(f"Total Solutions - {len(solutions)}.")
